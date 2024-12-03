5065212174089866de0e5832d3c7dbfbdedbb189
Guilhermex125 <125934449+Guilhermex125@users.noreply.github.com>
2024-12-02 22:59:15 -0300

    SITE
    SITE AMOR

---
+++ Script.js
@@ -0,0 +1,15 @@
+function carregarInstagram() {
+  //document representa o html
+  document.getElementById('instagram').innerHTML =
+    '<a class = "corlink" href="https://instagram.com/guilhermee_ferrreiraa?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D">Instagram</a>'
+}
+
+function carregarTikTok() {
+  document.getElementById('tiktok').innerHTML =
+    '<a class = "corlink" href="https://l.instagram.com/?u=http%3A%2F%2Ftiktok.com%2F%40guilherme_violin&e=AT3HhTE_yrLu7__SpluGG0r_b26eihVYpzqWR8ILV_98ejqWvRxFW3vfTmuQ3D9Jag59cZsrCC1FALQaIhv46AJzgcBb8nP7VMZVPQ">TikTok</a>'
+}
+
+function carregarLinkedin() {
+  document.getElementById('linkedin').innerHTML =
+    '<a class = "corlink" href="https://www.linkedin.com/in/guilherme-ferreira-76352b246">Linkedin</a>'
+}
